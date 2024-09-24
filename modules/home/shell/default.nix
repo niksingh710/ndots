0a12c5e7092a5719dcf09eec4e426e3109fd893a
niksingh710 <nik.singh710@gmail.com>
2024-09-25 03:08:03 +0530

    Added btop

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -1,5 +1,15 @@
-{
+{ lib, ... }:
+with lib; {
   imports = with builtins;
     map (fn: ./${fn})
-      (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+    (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+
+  programs.btop = {
+    enable = mkDefault true;
+    settings = {
+      theme_background = mkForce false;
+      vim_keys = true;
+    };
+  };
+
 }
