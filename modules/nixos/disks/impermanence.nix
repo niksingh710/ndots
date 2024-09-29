00e5b1eb4e32358987cc2ef75e223c3e9c9bacdc
niksingh710 <nik.singh710@gmail.com>
2024-09-30 02:48:38 +0530

    systemd script for encrypted device

--- modules/nixos/disks/impermanence.nix
+++ modules/nixos/disks/impermanence.nix
@@ -18,8 +18,17 @@ in {
     security.sudo.extraConfig = "Defaults lecture=never";
     fileSystems."/persistent".neededForBoot = true;
 
-    boot.initrd.postDeviceCommands = lib.mkAfter # bash
-      ''
+    boot.initrd.systemd.services.rollback = {
+      description = "Rollback BTRFS root subvolume to a pristine state";
+      wantedBy = [ "initrd.target" ];
+      after = [
+        # LUKS/TPM process
+        "systemd-cryptsetup@enc.service"
+      ];
+      before = [ "sysroot.mount" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = ''
         mkdir /btrfs_tmp
         mount /dev/disk/by-partlabel/disk-primary-root /btrfs_tmp
         if [[ -e /btrfs_tmp/root ]]; then
@@ -43,6 +52,34 @@ in {
         btrfs subvolume create /btrfs_tmp/root
         umount /btrfs_tmp
       '';
+    };
+
+    # boot.initrd.postDeviceCommands = lib.mkAfter # bash
+    #   ''
+    #     mkdir /btrfs_tmp
+    #     mount /dev/disk/by-partlabel/disk-primary-root /btrfs_tmp
+    #     if [[ -e /btrfs_tmp/root ]]; then
+    #         mkdir -p /btrfs_tmp/old_roots
+    #         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+    #         mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    #     fi
+    #
+    #     delete_subvolume_recursively() {
+    #         IFS=$'\n'
+    #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+    #             delete_subvolume_recursively "/btrfs_tmp/$i"
+    #         done
+    #         btrfs subvolume delete "$1"
+    #     }
+    #
+    #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+    #         delete_subvolume_recursively "$i"
+    #     done
+    #
+    #     btrfs subvolume create /btrfs_tmp/root
+    #     umount /btrfs_tmp
+    #   '';
+
     environment.persistence."/persistent" = {
       enable = true; # NB: Defaults to true, not needed
       hideMounts = true;
