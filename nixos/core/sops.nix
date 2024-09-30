1199bf9952692f3e243a6bf54f84b11602250868
niksingh710 <nik.singh710@gmail.com>
2024-09-30 16:00:26 +0530

    quick fixes

--- nixos/core/sops.nix
+++ nixos/core/sops.nix
@@ -10,7 +10,7 @@ in {
     persist.dir = [ "/var/lib/sops/age" ];
     sops = {
       defaultSopsFile = ../../secrets/keys.yaml;
-      age.keyFile = "/var/lib/sops/age/keys.txt";
+      age.keyFile = "/persistent/var/lib/sops/age/keys.txt";
 
       secrets = {
         "private-keys/ssh" = {
