f718a1b3ea24492beabd74463369e6c86072df13
niksingh710 <nik.singh710@gmail.com>
2024-12-23 15:30:59 +0530

    flake: required update

--- nixos/optional/fprint.nix
+++ nixos/optional/fprint.nix
@@ -5,12 +5,12 @@
       enable = true;
     };
     udev.packages = with pkgs.custom;[
-      libfprint-goodixtls-55x4
+      # libfprint-goodixtls-55x4
     ];
   };
   security.pam.services.hyprlock.fprintAuth = true;
   environment.systemPackages = with pkgs.custom; [
     # FIXME: This is notworking as expected (used to work on Arch Linux)
-    libfprint-goodixtls-55x4
+    # libfprint-goodixtls-55x4
   ];
 }
