eead3a40e6d444eca333e222944e5fc27282442d
niksingh710 <nik.singh710@gmail.com>
2025-02-15 01:48:49 +0530

    gh: added sops key for `machine:mach`
    if GITHUB_TOKEN is available in the env it will access that

--- hosts/nixos/mach/mods.nix
+++ hosts/nixos/mach/mods.nix
@@ -1,8 +1,9 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, lib, ... }:
 let
   minimal-tmux = inputs.minimal-tmux.packages.${pkgs.system}.default;
 in
 {
+
   hm = {
 
     stylix.targets.fzf.enable = false;
@@ -18,5 +19,16 @@ in
         set -g @minimal-tmux-left-arrow ""
       '';
     }];
+    sops.secrets."private-keys/github_token" = { };
+    programs.gh.package = lib.mkForce (pkgs.symlinkJoin {
+      name = "gh";
+      paths = [ pkgs.gh ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/gh \
+          --run 'export GITHUB_TOKEN=''${GITHUB_TOKEN:-$(cat ${config.hm.sops.secrets."private-keys/github_token".path})}'
+      '';
+      meta.mainProgram = "gh";
+    });
   };
 }
