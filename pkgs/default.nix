cbb0cff4a5e1bea98d0ce97ed07696eb3ffbae30
niksingh710 <nik.singh710@gmail.com>
2025-03-08 20:05:53 +0530

    zen-browser: packaged zen browser appimage
    * Not added in impermanence config as waiting for pr to merge

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@
   minecraft = pkgs.callPackage ./minecraft { };
   road-rage = pkgs.callPackage ./road-rage { };
   wl-ocr = pkgs.callPackage ./wl-ocr { };
+  zen-browser-appimage = pkgs.callPackage ./zen-browser-appimage { };
 }
---
+++ pkgs/zen-browser-appimage/default.nix
@@ -0,0 +1,30 @@
+# Packaged appimage till the build time on nixpkgs reduced.
+# https://github.com/NixOS/nixpkgs/issues/327982
+{ appimageTools, fetchurl, ... }:
+let
+  pname = "zen";
+  version = "latest";
+
+  src = fetchurl {
+    url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
+    sha256 = "sha256-hZiJ8JLzLhtD1W8DAso3yBAJYhFE+nJEbQJa59AWjnU="; # Remember as in url `/latest/` is used the sha256 will change with releases.
+  };
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    # Install .desktop file
+    install -m 444 -D ${appimageContents}/zen.desktop $out/share/applications/${pname}.desktop
+    # Install icon
+    install -m 444 -D ${appimageContents}/zen.png $out/share/icons/hicolor/128x128/apps/${pname}.png
+  '';
+
+
+  meta.platforms = [ "x86_64-linux" ];
+
+}
