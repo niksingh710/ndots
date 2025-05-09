40d539cf2f0fbc487fd83d68864ec5398cc94179
niksingh710 <nik.singh710@gmail.com>
2025-05-09 11:28:21 +0530

    build: updated flake and fixes to support the update

--- modules/home/nixos/mako.nix
+++ modules/home/nixos/mako.nix
@@ -11,25 +11,25 @@ in
 {
   services.mako = {
     enable = true;
-    anchor = "bottom-right";
-    borderRadius = if opts.rounding then 8 else 0;
-    # backgroundColor = "#${colors.base00}";
-    borderColor = mkForce "#${colors.base06}";
-    # progressColor = "#${colors.base03}";
-    # textColor = "#${colors.base03}";
-    defaultTimeout = 6000;
-    margin = "8";
-    padding = "4";
 
-    extraConfig = # ini
-      ''
-        outer-margin=0,0,8,0
-        [urgency=low]
-        border-color=#${colors.base08}
-        [mode=dnd]
-        invisible=1
-        on-notify=none
-      '';
+    settings = {
+      anchor = "bottom-right";
+      border-radius = if opts.rounding then 8 else 0;
+      border-color = mkForce "#${colors.base06}";
+      default-timeout = 6000;
+      margin = "8";
+      padding = "4";
+      outer-margin = "0,0,8,0";
+    };
+    criteria = {
+      "urgency=low" = {
+        border-color = lib.mkForce "#${colors.base08}";
+      };
+      "mode=dnd" = {
+        invisible = "true";
+        on-notify = "none";
+      };
+    };
   };
 
   wayland.windowManager.hyprland.settings.bind = [
