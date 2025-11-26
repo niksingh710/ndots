2e42e071338b949f2e0f14556b3fb76cdc6b9c0c
Nikhil Singh <nik.singh710@gmail.com>
2025-11-26 21:08:54 +0530

    fix: disk config generated

---
+++ hosts/nixos/virt/file.nix
@@ -0,0 +1,126 @@
+{
+  disko = {
+    devices = {
+      disk = {
+        primary = {
+          content = {
+            partitions = {
+              _type = "merge";
+              contents = [
+                {
+                  ESP = {
+                    content = {
+                      format = "vfat";
+                      mountOptions = [
+                        "defaults"
+                        "umask=0077"
+                      ];
+                      mountpoint = "/boot";
+                      type = "filesystem";
+                    };
+                    size = "512M";
+                    type = "EF00";
+                  };
+                }
+                {
+                  _type = "if";
+                  condition = true;
+                  content = {
+                    root = {
+                      content = {
+                        subvolumes = {
+                          _type = "merge";
+                          contents = [
+                            {
+                              "/nix" = {
+                                mountOptions = [
+                                  "compress=zstd"
+                                  "noatime"
+                                  "noacl"
+                                ];
+                                mountpoint = "/nix";
+                              };
+                              "/root" = {
+                                mountOptions = [ "compress=zstd" ];
+                                mountpoint = "/";
+                              };
+                            }
+                            {
+                              _type = "if";
+                              condition = false;
+                              content = {
+                                "/persistent" = {
+                                  mountOptions = [ "compress=zstd" ];
+                                  mountpoint = "/persistent";
+                                };
+                              };
+                            }
+                          ];
+                        };
+                        type = "btrfs";
+                      };
+                      size = "100%";
+                      type = "8300";
+                    };
+                  };
+                }
+                {
+                  _type = "if";
+                  condition = false;
+                  content = {
+                    root = {
+                      content = {
+                        content = {
+                          extraArgs = [ "-f" ];
+                          subvolumes = {
+                            _type = "merge";
+                            contents = [
+                              {
+                                "/nix" = {
+                                  mountOptions = [
+                                    "compress=zstd"
+                                    "noatime"
+                                    "noacl"
+                                  ];
+                                  mountpoint = "/nix";
+                                };
+                                "/root" = {
+                                  mountOptions = [ "compress=zstd" ];
+                                  mountpoint = "/";
+                                };
+                              }
+                              {
+                                _type = "if";
+                                condition = false;
+                                content = {
+                                  "/persistent" = {
+                                    mountOptions = [ "compress=zstd" ];
+                                    mountpoint = "/persistent";
+                                  };
+                                };
+                              }
+                            ];
+                          };
+                          type = "btrfs";
+                        };
+                        name = "cryptroot";
+                        settings = {
+                          allowDiscards = true;
+                        };
+                        type = "luks";
+                      };
+                      size = "100%";
+                    };
+                  };
+                }
+              ];
+            };
+            type = "gpt";
+          };
+          device = "/dev/sda";
+          type = "disk";
+        };
+      };
+    };
+  };
+}
