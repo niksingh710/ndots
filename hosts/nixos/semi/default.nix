1ba71282d1f89681f5d37026b06b8392dfaa5b8e
Nikhil Singh <nik.singh710@gmail.com>
2025-12-04 16:51:36 +0530

    chore: adding semi host

---
+++ hosts/nixos/semi/default.nix
@@ -0,0 +1,77 @@
+{
+  flake,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  me = (import (flake + "/config.nix")).me // {
+    username = "nikhil.singh";
+  };
+in
+{
+  imports = [
+    (lib.mkAliasOptionModule [ "hm" ] [ "home-manager" "users" me.username ])
+    flake.nixosModules.default
+
+    # Important for the hardware
+    flake.inputs.disko.nixosModules.disko
+    ./disk.nix
+    # should be generated sudo nixos-generate-config --show-hardware-config --root /mnt > ./hosts/nixos/{host}/hardware.nix>
+    ./hardware.nix
+  ];
+
+  programs.zsh.enable = true;
+  # Primary user setup
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.${me.username} = {
+      name = me.username;
+      home = "/home/${me.username}";
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+      ];
+      openssh.authorizedKeys.keys = me.sshPublicKeys;
+    };
+  };
+  services.tailscale.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+    extraConfig = # sshd_config
+      ''
+        AcceptEnv LANG LC_* ANTHROPIC_MODEL ANTHROPIC_AUTH_TOKEN ANTHROPIC_BASE_URL CLAUDE_CODE_DISABLE_EXPERIMENTAL_BETAS
+      '';
+  };
+
+  networking = {
+    networkmanager.enable = true;
+
+    # This does not disable wifi
+    # It is just an minimal alternative to Network manager
+    wireless.enable = false;
+  };
+
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot";
+  };
+  security.sudo.wheelNeedsPassword = false;
+
+  nix.settings.trusted-users = [ me.username ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = "25.11";
+}
---
+++ hosts/nixos/semi/users/nikhil.singh.nix
@@ -0,0 +1,58 @@
+# Contains override for packages/moduels
+# Most of my modules are meant to be used by multiple users
+# and multiple people online.
+# here in this config file i override them according to my needs
+{
+  flake,
+  pkgs,
+  ...
+}:
+let
+  me = (import (flake + "/config.nix")).me // {
+    username = "nikhil.singh";
+  };
+in
+{
+  # users specific home modules
+  imports = [ flake.homeModules.sops ];
+
+  # comes from homeModules.editor
+  nvix.variant = "core";
+
+  # Color override for tmux plugin
+  programs.tmux.plugins = [
+    {
+      plugin = pkgs.tmuxPlugins.minimal-tmux-status;
+      extraConfig = ''
+        set -g @minimal-tmux-use-arrow true
+        set -g @minimal-tmux-right-arrow ""
+        set -g @minimal-tmux-left-arrow ""
+      '';
+    }
+  ];
+
+  programs.git = {
+    settings = {
+      user = {
+        name = me.fullname;
+        email = me.email;
+      };
+    };
+    includes = [
+      {
+        condition = "gitdir:~/work/bitbucket/";
+        contents.user.email = "${me.username}@juspay.in";
+      }
+    ];
+  };
+
+  # git override for my personal/work email setup
+  programs.git = {
+    settings = {
+      users = {
+        name = me.fullname;
+        email = me.email;
+      };
+    };
+  };
+}
