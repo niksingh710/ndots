713106b9fd6e0bceba732dbf986bfa5fe3ac0055
Nikhil Singh <nik.singh710@gmail.com>
2026-03-25 13:43:02 +0530

    update: nvix

--- hosts/nixos/semi/hardware.nix
+++ hosts/nixos/semi/hardware.nix
@@ -4,6 +4,7 @@
 {
   config,
   lib,
+  pkgs,
   modulesPath,
   ...
 }:
@@ -13,17 +14,21 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
--- modules/home/shell/sesh.nix
+++ modules/home/shell/sesh.nix
@@ -6,12 +6,31 @@
     tmuxKey = "c-o";
     settings = {
       blacklist = [ "scratch" ];
+      dir_length = 2;
       session = [
         rec {
           name = "todo";
           path = "~/.todo/todo.md";
           startup_command = "nvim ${path}";
           preview_command = "${lib.getExe pkgs.glow} ${path}";
+        }
+        rec {
+          name = "notes";
+          path = "~/.notes";
+          startup_command = # sh
+            ''
+              nvim --cmd 'let g:auto_session_enabled = v:false' -c 'cd ~/.notes/' -c 'lua vim.schedule(function() vim.cmd("ObsidianQuickSwitch") end)'
+            '';
+          preview_command = "${lib.getExe pkgs.fzf-preview} ${path}";
+        }
+        rec {
+          name = "LeetCode";
+          path = "~";
+          startup_command = # sh
+            ''
+              nvim --cmd 'let g:auto_session_enabled = v:false' -c 'cd ~' -c 'Leet'
+            '';
+          preview_command = "${lib.getExe pkgs.fzf-preview} ${path}";
         }
         {
           name = "main";
