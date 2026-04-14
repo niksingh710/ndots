a94ea86f937b778746748d07a46c5bfc8f2307e6
Nikhil Singh <nik.singh710@gmail.com>
2026-04-14 14:12:23 +0530

    feat(zsh): add OSC52 clipboard integration for zsh-vi-mode
    - Add OSC52 clipboard support working over SSH and tmux
    - Integrate with zsh-vi-mode for yy, yw, y$, and visual yank
    - Clear visual mode highlight immediately after yanking
    - Reference: https://github.com/jeffreytse/zsh-vi-mode/issues/329

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -1,12 +1,22 @@
 { pkgs, lib, ... }:
 let
-  zshSysClip =
-    lib.optionalString pkgs.stdenv.hostPlatform.isLinux # sh
-      ''
-        export ZSH_SYSTEM_CLIPBOARD_USE_WL_CLIPBOARD="wl-clipboard"
-      '';
-  copy = pkgs.writeShellScriptBin "copy" ''
-    printf "\033]52;c;%s\007" "$(base64 | tr -d '\n')"
+  copyScript = pkgs.writeText "copy-script" ''
+    #!/usr/bin/env bash
+    text=$(cat)
+    if [ -n "''${TMUX:-}" ]; then
+      encoded=$(printf '%s' "$text" | ${pkgs.coreutils}/bin/base64 | tr -d '\n')
+      printf '\ePtmux;\e\033]52;c;%s\a\e\\' "$encoded"
+    else
+      encoded=$(printf '%s' "$text" | ${pkgs.coreutils}/bin/base64 | tr -d '\n')
+      printf '\033]52;c;%s\007' "$encoded"
+    fi
+  '';
+
+  copy = pkgs.runCommand "copy" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+    mkdir -p $out/bin
+    cp ${copyScript} $out/bin/copy
+    chmod +x $out/bin/copy
+    patchShebangs $out/bin/copy
   '';
 
 in
@@ -20,14 +30,12 @@ in
       append = true;
       expireDuplicatesFirst = true;
     };
-    profileExtra = # sh
-      ''
-        ${zshSysClip}
-      '';
+    sessionVariables = {
+      ZVM_SYSTEM_CLIPBOARD_ENABLED = "true";
+      ZVM_CLIPBOARD_COPY_CMD = "${copy}/bin/copy";
+    };
     localVariables = {
       ZVM_VI_INSERT_ESCAPE_BINDKEY = "jk";
-      ZVM_CLIPBOARD_COPY_CMD = "${lib.getExe copy}";
-      ZVM_SYSTEM_CLIPBOARD_ENABLED = "true";
     };
     plugins = [
       {
@@ -41,15 +49,65 @@ in
         file = "share/fzf-tab/fzf-tab.plugin.zsh";
       }
     ];
-    initContent =
-      lib.mkOrder 1500 # sh
-        ''
-          # `ctrl + j` and `ctrl + k` to navigate through suggestions
-          bindkey '^[j' fzf-tab-complete
-          bindkey '^[k' fzf-tab-complete
+    initContent = lib.mkOrder 1500 ''
+      bindkey '^[j' fzf-tab-complete
+      bindkey '^[k' fzf-tab-complete
 
-          # user can add session based alias and fn for quick use
-          [ -f "$HOME/.temp.zsh" ] && source "$HOME/.temp.zsh"
-        '';
+      # OSC52 Clipboard Integration
+      # Copies yanked text to system clipboard via OSC52 (works over SSH and tmux)
+      # Note: Visual mode highlight may linger after yanking due to upstream zsh-vi-mode bug
+      # https://github.com/jeffreytse/zsh-vi-mode/issues/329
+      _osc52_copy() {
+        local text="$1"
+        local encoded=$(printf '%s' "$text" | base64 | tr -d '\n')
+        if [[ -n "$TMUX" ]]; then
+          printf '\ePtmux;\e\033]52;c;%s\a\e\\' "$encoded"
+        else
+          printf '\033]52;c;%s\007' "$encoded"
+        fi
+      }
+
+      _yank_line_with_osc52() {
+        zle vi-yank-whole-line
+        _osc52_copy "$CUTBUFFER"
+      }
+
+      _normal_yank_with_osc52() {
+        zle vi-yank
+        _osc52_copy "$CUTBUFFER"
+      }
+
+      _visual_yank_with_osc52() {
+        zle copy-region-as-kill
+        _osc52_copy "$CUTBUFFER"
+        # Force exit visual mode and clear highlight
+        REGION_ACTIVE=0
+        if (( $+functions[zvm_exit_visual_mode] )); then
+          zvm_exit_visual_mode
+        fi
+        if (( $+ZVM_REGION_HIGHLIGHT )); then
+          ZVM_REGION_HIGHLIGHT=()
+        fi
+        region_highlight=()
+        # Force immediate refresh (clears highlight, moves cursor temporarily)
+        zle -U ' '
+        zle backward-delete-char
+        zle -R
+      }
+
+      zle -N _yank_line_with_osc52
+      zle -N _normal_yank_with_osc52
+      zle -N _visual_yank_with_osc52
+
+      bindkey -M vicmd 'yy' _yank_line_with_osc52
+      bindkey -M vicmd 'y' _normal_yank_with_osc52
+      bindkey -M visual 'y' _visual_yank_with_osc52
+
+      function zvm_after_lazy_keybindings() {
+        bindkey -M visual 'y' _visual_yank_with_osc52
+      }
+
+      [ -f "$HOME/.temp.zsh" ] && source "$HOME/.temp.zsh"
+    '';
   };
 }
